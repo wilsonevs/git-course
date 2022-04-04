1d58765b43583117931d6eb6a3fe6c04f5c1b1fe
wilsonevs <wilsonevs@gmail.com>
2022-04-04 01:18:48 -0500

    Arreglo de bug en la linea 6 del archivo Main..java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         
         String nombre=  "Wilson Valencia";
-        System.out.printf("Hclsola %s%n",nombre);
+        System.out.printf("Hola %s%n",nombre);
 
         suma(5,5);
 
