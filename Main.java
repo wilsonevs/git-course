ee628542c7c1b77e5a9ad0feb78338ffff96de59
wilsonevs <wilsonevs@gmail.com>
2022-04-04 01:06:29 -0500

    Feature: creación de metodo de suma

--- Main.java
+++ Main.java
@@ -2,9 +2,14 @@
 public class Main {
     public static void main(String[] args) {
         
-
         String nombre=  "Wilson Valencia";
         System.out.printf("Hclsola %s%n",nombre);
 
+        suma(5,5);
+
+    }
+
+    public static void suma(Integer numUno, Integer numDos){
+        System.out.printf("%s + %s = %s%n", numUno, numDos, (numUno+numDos));
     }
 }
